d5c5eb0663f4c9afebe1f0db8756358c7eefe269
Vliet van, SM (Sander) <sander.van.vliet@rabobank.com>
2016-07-08 16:37:44 +0200

    Implementing the tool window

---
+++ JSCrunch.VisualStudio/ProcessingQueueControl.xaml.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ProcessingQueueControl.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using MsVsShell = Microsoft.VisualStudio.Shell;
+using IOleServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
+
+namespace JSCrunch.VisualStudio
+{
+    /// <summary>
+    ///     Interaction logic for ProcessingQueueControl.
+    /// </summary>
+    public partial class ProcessingQueueControl : UserControl, IVsRunningDocTableEvents
+    {
+        private uint _rdtCookie;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ProcessingQueueControl" /> class.
+        /// </summary>
+        public ProcessingQueueControl()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessingQueueControl_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var serviceProvider = Package.GetGlobalService(typeof(IOleServiceProvider)) as IOleServiceProvider;
+            if (serviceProvider == null)
+            {
+                return;
+            }
+
+            var rdt = new RunningDocumentTable(new ServiceProvider(serviceProvider));
+            if (rdt == null)
+            {
+                return;
+            }
+
+            _rdtCookie = rdt.Advise(this);
+
+        }
+
+        public int OnAfterFirstDocumentLock(uint docCookie, uint dwRDTLockType, uint dwReadLocksRemaining, uint dwEditLocksRemaining)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeLastDocumentUnlock(uint docCookie, uint dwRDTLockType, uint dwReadLocksRemaining, uint dwEditLocksRemaining)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterSave(uint docCookie)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterAttributeChange(uint docCookie, uint grfAttribs)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeDocumentWindowShow(uint docCookie, int fFirstShow, IVsWindowFrame pFrame)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterDocumentWindowHide(uint docCookie, IVsWindowFrame pFrame)
+        {
+            return VSConstants.S_OK;
+        }
+    }
+}
---
+++ JSCrunch.VisualStudio/ProcessingQueuePackage.cs
@@ -0,0 +1,76 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ProcessingQueuePackage.cs" company="Microsoft">
+//     Copyright (c) Microsoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace JSCrunch.VisualStudio
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(ProcessingQueue))]
+    [Guid(ProcessingQueuePackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class ProcessingQueuePackage : Package
+    {
+        /// <summary>
+        /// ProcessingQueuePackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "33b97e83-89d6-441d-ad4e-9e7dc0f828d1";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProcessingQueue"/> class.
+        /// </summary>
+        public ProcessingQueuePackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            ProcessingQueueCommand.Initialize(this);
+            base.Initialize();
+        }
+
+        #endregion
+    }
+}
