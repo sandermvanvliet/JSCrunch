019105bab7d9698d19945886e8ae5b487a529655
sandermvanvliet <sander@codenizer.nl>
2016-09-03 09:10:55 +0200

    Initialize tests collection

--- JSCrunch.VisualStudio/Metadata/ProjectModel.cs
+++ JSCrunch.VisualStudio/Metadata/ProjectModel.cs
@@ -4,7 +4,13 @@ namespace JSCrunch.VisualStudio.Metadata
 {
     public class ProjectModel
     {
+        public ProjectModel()
+        {
+            Tests = new ObservableCollection<string>();
+        }
+
         public ObservableCollection<string> Tests { get; set; }
+
         public string Name { get; set; }
     }
 }
